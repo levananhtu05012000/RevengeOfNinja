c20006fbb7a6e99e8bc6158b09cb1fefd3238206
ninh137 <adzekk01@gmail.com>
2022-07-21 10:45:46 +0700

    Fix audio

--- Assets/Scripts/Menu/AudioManager.cs
+++ Assets/Scripts/Menu/AudioManager.cs
@@ -19,7 +19,6 @@ public static class AudioManager
         audioClips.Add(AudioClipName.Music, Resources.Load<AudioClip>("MysteriousLongM"));
         audioClips.Add(AudioClipName.Button, Resources.Load<AudioClip>("ButtonClick"));
         audioClips.Add(AudioClipName.Dash, Resources.Load<AudioClip>("ButtonClick"));
-        audioClips.Add(AudioClipName.Jump, Resources.Load<AudioClip>("ButtonClick"));
         audioClips.Add(AudioClipName.Slice, Resources.Load<AudioClip>("ButtonClick"));
         audioClips.Add(AudioClipName.Hit, Resources.Load<AudioClip>("ButtonClick"));
         audioClips.Add(AudioClipName.Up, Resources.Load<AudioClip>("ButtonClick1"));
@@ -27,7 +26,8 @@ public static class AudioManager
         audioClips.Add(AudioClipName.FleshTakehit, Resources.Load<AudioClip>("flesh-takehit"));
         audioClips.Add(AudioClipName.SkeletonTakehit, Resources.Load<AudioClip>("skeleton-takehit"));
         audioClips.Add(AudioClipName.SkeletonDie, Resources.Load<AudioClip>("skeleton-die"));
-        audioClips.Add(AudioClipName.Explosion, Resources.Load<AudioClip>("Explosion"));
+        //audioClips.Add(AudioClipName.Explosion, Resources.Load<AudioClip>("Explosion"));        // Big Explosion 
+        audioClips.Add(AudioClipName.Explosion, Resources.Load<AudioClip>("Explosion-smaller"));
         audioClips.Add(AudioClipName.GoblinAttack, Resources.Load<AudioClip>("goblin-attack"));
         audioClips.Add(AudioClipName.MushroomDetection, Resources.Load<AudioClip>("mushroom-detection"));
         audioClips.Add(AudioClipName.TrapAttack, Resources.Load<AudioClip>("trap-attack"));
@@ -39,6 +39,8 @@ public static class AudioManager
         audioClips.Add(AudioClipName.PlayerDie, Resources.Load<AudioClip>("player-die"));
         //audioClips.Add(AudioClipName.PlayerDie, Resources.Load<AudioClip>("player-die-1"));
         audioClips.Add(AudioClipName.PlayerDash, Resources.Load<AudioClip>("player-dash"));
+        //audioClips.Add(AudioClipName.Jump, Resources.Load<AudioClip>("ButtonClick"));
+        audioClips.Add(AudioClipName.Jump, Resources.Load<AudioClip>("player-jump"));
 
         initialized = true;
     }
